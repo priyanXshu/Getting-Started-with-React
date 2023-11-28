36e1d883efb53fb0889cc13a01b4e6b0b0d78491
Priyanshu Singh <priyanshusinghajm@gmail.com>
2023-11-28 19:48:53 +0530

    todo context

---
+++ todoList/src/context/TodoContext.js
@@ -0,0 +1,20 @@
+import { createContext, useContext } from "react";
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Todo mssg",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const Todoprovider = TodoContext.Provider
